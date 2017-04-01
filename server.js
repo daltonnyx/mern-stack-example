77fa83281308b242c74b9bca68a88909d814abe7
daltonnyx <quytruong851@gmail.com>
2017-04-01 23:09:50 +0700

    Create Static File

---
+++ server.js
@@ -0,0 +1,10 @@
+//Load modules
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('static'));
+
+app.listen(3000,function(){
+    console.log("App started at port 3000");
+});
